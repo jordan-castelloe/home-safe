3555612075e9f6e1b8e997e4b5c1cf27a9287f44
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-05-07 11:36:33 -0500

    logged current time with moment.js

--- app/client/trip-clock.js
+++ app/client/trip-clock.js
@@ -21,13 +21,15 @@ const showTripProgress = () => {
 
 // is there any reason to use local storage here??
 const startTimer = () => {
-  const trip = localStorage.getItem("trip");
-  const returnTime = JSON.parse(trip.returnTime);
-  console.log(returnTime);
+  const trip = JSON.parse(localStorage.getItem("trip"));
+  const returnTime = moment(trip.returnTime, "hh:mm:ss A")
+  console.log('moment obj', returnTime);
+  const currentHour =  parseInt(moment().format('H'));
+  const currentMinute = parseInt(moment().format('mm'));
+  console.log('current hour: current minute', `${currentHour}:${currentMinute}`);
 }
 
-
-// Conglomerate function that fires all the other functions
+// Parent function that fires all the other functions
 const startTrip = () => {
   storeTrip();
   showTripProgress();
@@ -35,3 +37,51 @@ const startTrip = () => {
 }
 
 $('#start-trip').click(startTrip);
+
+
+// function displayTime() {
+//   $('.clock').text(moment().format('H:mm:ss a'));
+//   setTimeout(displayTime, 1000);
+// }
+
+// function clearForm() {
+//   document.getElementById("alarm-form").reset();
+// }
+
+// var sound = document.getElementById("sound");
+
+// function playAlarm() {
+//   sound.play();
+// }
+
+// function snoozeAlarm() {
+//   clearInterval(myVar);
+// }
+
+
+// $(document).ready(function () {
+//   //when page is loaded run displayTime
+//   displayTime();
+//   $('#alarm').hide();
+
+//   $('#alarm-form').submit(function (event) {
+//     event.preventDefault();
+
+//     var hour = parseInt($('#hour').val());
+//     var minute = parseInt($('#minute').val());
+//     $(".alarm-set").text("Your alarm is set for " + hour + ":" + minute);
+
+//     function alarmTimer() {
+//       var currentHour = parseInt(moment().format('H'));
+//       var currentMinute = parseInt(moment().format('mm'));
+
+//       if ((hour === currentHour) && (minute === currentMinute)) {
+//         $('#alarm').show();
+//         $('.alarm-set').hide();
+//         playAlarm();
+//       }
+//     }
+//     var myVar = setInterval(function () { alarmTimer() }, 1000);
+//     // setInterval(alarmTimer, 1000);
+//   });
+// });
