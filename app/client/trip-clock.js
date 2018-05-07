5c75d16007eae8db34e3e512083bec29d921b07e
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-05-07 14:04:29 -0500

    timer functions

--- app/client/trip-clock.js
+++ app/client/trip-clock.js
@@ -22,73 +22,34 @@ const showTripProgress = () => {
   $('.trip-in-progress').show();
 }
 
-// is there any reason to use local storage here??
-const checkTime = () => {
-  // Grab return time values
-  const { returnHour, returnMinute, returnTimeOfDay } = getTrip();
-  
-  // Grab values for current Time
-  const currentHour =  parseInt(moment().format('hh'));
-  const currentMinute = parseInt(moment().format('mm'));
-  const currentTimeOfDay = moment().format('A');
-  console.log('return time: return minute', `${returnHour}:${returnMinute} ${returnTimeOfDay}`);
-  console.log('current hour: current minute', `${currentHour}:${currentMinute} ${currentTimeOfDay}`);
 
+// starts the interval
+const startTimer = () => {
+  const timer = setInterval(() => {
+    // Grab return time values
+    const { returnHour, returnMinute, returnTimeOfDay } = getTrip();
 
+    // Grab values for current Time
+    const currentHour = parseInt(moment().format('hh'));
+    const currentMinute = parseInt(moment().format('mm'));
+    const currentTimeOfDay = moment().format('A');
+
+    console.log('return time: return minute', `${returnHour}:${returnMinute} ${returnTimeOfDay}`);
+    console.log('current hour: current minute', `${currentHour}:${currentMinute} ${currentTimeOfDay}`);
+
+    if (returnHour == currentHour && returnMinute == currentMinute && returnTimeOfDay == currentTimeOfDay) {
+      clearInterval(timer);
+      console.log("Timer done!!");
+    }
+  }, 1000);
 }
 
 // Parent function that fires all the other functions
 const startTrip = () => {
   showTripProgress();
-  setInterval(checkTime, 1000);
+  startTimer();
 }
 
 $('#start-trip').click(startTrip);
 
 
-// function displayTime() {
-//   $('.clock').text(moment().format('H:mm:ss a'));
-//   setTimeout(displayTime, 1000);
-// }
-
-// function clearForm() {
-//   document.getElementById("alarm-form").reset();
-// }
-
-// var sound = document.getElementById("sound");
-
-// function playAlarm() {
-//   sound.play();
-// }
-
-// function snoozeAlarm() {
-//   clearInterval(myVar);
-// }
-
-
-// $(document).ready(function () {
-//   //when page is loaded run displayTime
-//   displayTime();
-//   $('#alarm').hide();
-
-//   $('#alarm-form').submit(function (event) {
-//     event.preventDefault();
-
-//     var hour = parseInt($('#hour').val());
-//     var minute = parseInt($('#minute').val());
-//     $(".alarm-set").text("Your alarm is set for " + hour + ":" + minute);
-
-//     function alarmTimer() {
-//       var currentHour = parseInt(moment().format('H'));
-//       var currentMinute = parseInt(moment().format('mm'));
-
-//       if ((hour === currentHour) && (minute === currentMinute)) {
-//         $('#alarm').show();
-//         $('.alarm-set').hide();
-//         playAlarm();
-//       }
-//     }
-//     var myVar = setInterval(function () { alarmTimer() }, 1000);
-//     // setInterval(alarmTimer, 1000);
-//   });
-// });
