63bab52f2db654eb033d9b0a76778d1c085d7d91
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-05-07 10:52:01 -0500

    retrieved trip info from local storage

--- app/client/trip-clock.js
+++ app/client/trip-clock.js
@@ -1,13 +1,37 @@
 'use strict';
 
-const startTrip = () => {
+// TODO: change to css
+$('.trip-in-progress').hide();
+
+// Store trip in local storage
+const storeTrip = () => {
   const trip = {
     location: $('#location').val(),
     activity: $('#activity').val(),
     returnTime: $('#return-time').val()
   }
-  console.log('trip', trip);
   localStorage.setItem("trip", JSON.stringify(trip));
 }
 
-$('#start-trip').click(startTrip)
+// Hide the trip form and show the timer
+const showTripProgress = () => {
+  $('.start-trip').hide();
+  $('.trip-in-progress').show();
+}
+
+// is there any reason to use local storage here??
+const startTimer = () => {
+  const trip = localStorage.getItem("trip");
+  const returnTime = JSON.parse(trip.returnTime);
+  console.log(returnTime);
+}
+
+
+// Conglomerate function that fires all the other functions
+const startTrip = () => {
+  storeTrip();
+  showTripProgress();
+  startTimer();
+}
+
+$('#start-trip').click(startTrip);
