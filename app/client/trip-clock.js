4557973ce656e742f31d7eca017847efde2dd382
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-05-08 13:35:48 -0500

    deleted console logs

--- app/client/trip-clock.js
+++ app/client/trip-clock.js
@@ -32,8 +32,10 @@ const startTimer = () => {
     // Grab values for current Time
     const currentHour = parseInt(moment().format('hh'));
     let currentMinute = parseInt(moment().format('mm'));
-    currentMinute = currentMinute < 10 ? `0${currentMinute}` : currentMinute;
     const currentTimeOfDay = moment().format('A');
+
+    // if the current minute is less than 10, stick a zero in front of it
+    currentMinute = currentMinute < 10 ? `0${currentMinute}` : currentMinute;
 
     console.log('return time: return minute', `${returnHour}:${returnMinute} ${returnTimeOfDay}`);
     console.log('current hour: current minute', `${currentHour}:${currentMinute} ${currentTimeOfDay}`);
