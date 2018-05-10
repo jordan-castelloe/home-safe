5d35d18d179955eaf4b5fbeef15e7b286610a2a1
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-05-10 18:18:31 -0500

    converted to promise.all

--- app/controllers/tripCtrl.js
+++ app/controllers/tripCtrl.js
@@ -52,25 +52,22 @@ module.exports.getUserCodes = (req, res, next) => {
 
 // TODO: Send a different text msg if they used their emergency code
 
-const sendToTwilio = (userName, contactName, contactNumber, req) => {
+const sendToTwilio = (contactArray, req) => {
   const accountSid = 'ACcae2b80fc1398969262d1eb12bd61c29';
   const authToken = 'e4c62e21eb842505db451ce827091d7f';
   const twilio = require('twilio');
   const client = new twilio(accountSid, authToken);
-  const { activity, lat, long } = req.body;
-
-  const message = `Hi ${contactName}, your friend ${userName} went ${activity} and hasn't made it back in time. Their last known location is: ${lat} lat, ${long} long. Would you mind checking in on them?` 
-
-  console.log('message and number', message, contactNumber);
-  // client.messages.create({
-  //   body: message,
-  //   to: contactNumber,
-  //   from: '+18286685165'
-  // })
-  // .then(message => {
-  //   console.log(message.sid);
-  //   res.status(200).send("Texts sent!");
-  // })    
+  const { activity, lat, long } = req;
+  return Promise.all(
+    contactArray.map(contact => {
+      let message = `Hi ${contact.name}, your friend ${contact.first_name} went ${activity} and hasn't made it back in time. Their last known location is: ${lat} lat, ${long} long. Would you mind checking in on them?`
+      return client.messages.create({
+        body: message,
+        to: contact.phone_number,
+        from: '+18286685165'
+      })
+    })
+  )
 }
 
 
@@ -83,11 +80,16 @@ module.exports.sendTexts= (req, res, next) => {
   sequelize.query(
     `SELECT e.name, e.phone_number, u.first_name FROM "Emergency_Contacts" e JOIN "Users" u ON e.user_id = u.id WHERE u.id = ${req.user.id}`
   )
-  .spread( contacts => {
-    contacts.forEach(contact => {
-      console.log('contact', contact);
-      sendToTwilio(contact.first_name, contact.name, contact.phone_number, req);
-    })
+  .spread( contactArray => {
+    return sendToTwilio(contactArray, req.body)
+  })
+  .then(msg => {
+    console.log('msg', msg);
+    console.log('Success!');
+  })
+  .catch(err => {
+    console.log(err);
+    next(err);
   })
 }
 
