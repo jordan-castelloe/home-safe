85fd64d4586d6f6d5b9d15ebb35dfd7252f00d3b
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-05-14 15:55:19 -0500

    fixed delete button

--- app/client/events.js
+++ app/client/events.js
@@ -1,14 +1,20 @@
 'use strict';
 
+console.log('front-end.js hooked up!');
+
 $('.deleteContact').on('click', event => {
+  console.log('button clicked!');
   let contactId = $(event.target).attr("id");
+  console.log(contactId);
+  console.log(event.target);
   $.ajax({
     url: `/contacts/${contactId}`,
     type: 'DELETE',
   })
   .done(() => {
     console.log('event.target', event.target);
-    $(event.target).remove();
+    $(event.target).parents('.card').remove();
+    console.log($(event.target).parents('.card'));
   })
   .fail(err => {
     console.log('Something went wrong!', err);
