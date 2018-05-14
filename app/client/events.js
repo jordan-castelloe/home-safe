1a1338fa1b91c69089dfd09cdd97450b4460c288
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-05-14 17:31:42 -0500

    removed console logs

--- app/client/events.js
+++ app/client/events.js
@@ -1,20 +1,13 @@
 'use strict';
 
-console.log('front-end.js hooked up!');
-
 $('.deleteContact').on('click', event => {
-  console.log('button clicked!');
   let contactId = $(event.target).attr("id");
-  console.log(contactId);
-  console.log(event.target);
   $.ajax({
     url: `/contacts/${contactId}`,
     type: 'DELETE',
   })
   .done(() => {
-    console.log('event.target', event.target);
     $(event.target).parents('.card').remove();
-    console.log($(event.target).parents('.card'));
   })
   .fail(err => {
     console.log('Something went wrong!', err);
